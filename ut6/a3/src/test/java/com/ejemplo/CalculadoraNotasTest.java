65be78af0c16d5ec205f5269472de6db6d6bf6cd
Alberto <albertocarballohernandez@gmail.com>
2026-04-17 01:00:09 +0100

    Realización de la actividad a3

---
+++ ut6/a3/src/test/java/com/ejemplo/CalculadoraNotasTest.java
@@ -0,0 +1,32 @@
+package com.ejemplo;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraNotasTest {
+
+    @Test
+    void testMediaSimple(){
+        assertEquals(7, CalculadoraNotas.calcularMedia(new int[]{6,7,8}));
+    }
+
+    @Test
+    void testMediaDecimal(){
+        assertEquals(8.5, CalculadoraNotas.calcularMedia(new int[]{10,9,8,7}));
+    }
+
+    // Inserta los nuevos test a partir de esta línea
+
+    @Test
+    void testListaVacia(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[]{});
+        });
+    }
+
+    @Test
+    void testNotaFueraDeRango(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[]{5, 11, 3});
+        });
+    }
+}
